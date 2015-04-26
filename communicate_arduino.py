99d2856ff53e8db1cc7cac5e32f75851f800da27
Lee Gross <leegross@Lees-MacBook-Air.local>
2015-04-26 16:09:12 -0400

    more arduino code

--- communicate_arduino.py
+++ communicate_arduino.py
@@ -6,17 +6,30 @@ ser = serial.Serial('/dev/cu.usbmodem1411', 9600, timeout=1)  # open first seria
 #initialize simulation
 n = 3
 p = .5
-c = 2
-k = 1
+c = 3
+k = 2
 sim = ac.Simulator(n, p, c, k)
 results = sim.run_convergence_with_results()
 
-for timestep in results:
-	current_signal = timestep[0]
-	output = str(current_signal) + ","
-	for agent_strategy in timestep[1:]:
-		output += str(agent_strategy) + ","
-	ser.write(output)
-	print output
+# send parameters
+# params = "-1,"+str(n)
+# ser.write(params)
+# ser.write("\n")
+
+# for timestep in results:
+# 	current_signal = timestep[0]
+# 	output = str(current_signal) 
+# 	for agent_strategy in timestep[1:]:
+# 		if agent_strategy == -1:
+# 			output += str(9)
+# 		else:
+# 			output += str(agent_strategy)
+# 	ser.write(output)
+# 	print output
+
+ser.write("0000")
+ser.write("1191")
+ser.write("2000")
+ser.write("3009")
 
 ser.close()             # close port
--- testing/testing.ino
+++ testing/testing.ino
@@ -1,52 +1,53 @@
 #include <Adafruit_NeoPixel.h>
-#include <Console.h>
 
 #define PIN 6
 
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(64, PIN);
     
 uint32_t colors[3];
+int num_agents;
 
 void setup() {
+  Serial.begin(9600);
   strip.begin();
   strip.show();
   colors[0] = strip.Color(150,0,0);
   colors[1] = strip.Color(0,150,0);
   colors[2] = strip.Color(0,0,150);
   strip.setBrightness(64);
-  Console.begin();
-
 }
 
 void loop() {
-  //strip.setPixelColor(n, red, green, blue);
-//  strip.setPixelColor(11, 255, 0, 255);
-//  
-//  uint32_t magenta = strip.Color(255, 0, 255);
-//  strip.setPixelColor(13, magenta);
-//  strip.show();
-  
-  int num_agents = 3;
-  int agent_strategies[num_agents];
   if (Serial.available() > 0){
-    int signal = Serial.parseInt();
-    Console.print(signal);
-    Serial.read(); //skip comma
-    for (int i=0; i < num_agents; i++){
-      agent_strategies[i] = Serial.parseInt();
-      Console.print(" ");
-      Console.print(agent_strategies[i]);
-      if (agent_strategies[i] ==-1){
-        strip.setPixelColor(i, 150,150,150);
+    num_agents=3;
+    int signal = Serial.read()-48;
+    Serial.println(signal);
+
+    if (signal == -100000){
+      num_agents = Serial.parseInt();
+      Serial.println(num_agents);
+
+    }
+    else{
+      int agent_strategies[num_agents];
+      
+      //read strategies for timestep
+      for (int i=0; i < num_agents; i++){
+        agent_strategies[i] = (Serial.read() - 48);
+        Serial.print(i);
+        Serial.print(" ");
+        Serial.println(agent_strategies[i]);
+        
+        int current_led = signal*8 + i;
+        if (agent_strategies[i] ==9){ //if agent not trasmitting
+          strip.setPixelColor(current_led, 150,150,150); //no LED
+        }
+        else{
+          strip.setPixelColor(current_led, colors[agent_strategies[i]]);
+        }
       }
-      else{
-        strip.setPixelColor(i, colors[agent_strategies[i]]);
-      }
-      Serial.read(); //skip comma
     }
     strip.show();
-    Console.println();
-    delay(1000);
   }
  
   
