501fee8d12ec77b8acd80b671a1aa5f336d80504
Matt Susskind <msuss@mit.edu>
2015-04-27 20:50:04 -0400

    Code complete

--- communicate_arduino.py
+++ communicate_arduino.py
@@ -1,35 +1,36 @@
 import serial
 import anticoordination as ac
-ser = serial.Serial('/dev/cu.usbmodem1411', 9600, timeout=1)  # open first serial port
+import time
+ser = serial.Serial('/dev/cu.usbmodem1451', 9600, timeout=1)  # open first serial port
 
 
 #initialize simulation
-n = 3
+n = 8
 p = .5
-c = 3
-k = 2
+c = 6
+k = 8
 sim = ac.Simulator(n, p, c, k)
 results = sim.run_convergence_with_results()
 
 # send parameters
-# params = "-1,"+str(n)
-# ser.write(params)
-# ser.write("\n")
+params = "9"+str(n)+str(c)
+ser.write(params)
 
-# for timestep in results:
-# 	current_signal = timestep[0]
-# 	output = str(current_signal) 
-# 	for agent_strategy in timestep[1:]:
-# 		if agent_strategy == -1:
-# 			output += str(9)
-# 		else:
-# 			output += str(agent_strategy)
-# 	ser.write(output)
-# 	print output
 
-ser.write("0000")
-ser.write("1191")
-ser.write("2000")
-ser.write("3009")
+for timestep in results:
+	current_signal = timestep[0]
+	output = str(current_signal) 
+	for agent_strategy in timestep[1:]:
+		if agent_strategy == -1:
+			output += str(9)
+		else:
+			output += str(agent_strategy)
+	ser.write(output)
+	print output
+	time.sleep(.2)
+
+
+
+
 
 ser.close()             # close port
--- testing/testing.ino
+++ testing/testing.ino
@@ -4,28 +4,30 @@
 
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(64, PIN);
     
-uint32_t colors[3];
+uint32_t colors[6];
 int num_agents;
+int num_channels;
 
 void setup() {
   Serial.begin(9600);
   strip.begin();
   strip.show();
-  colors[0] = strip.Color(150,0,0);
-  colors[1] = strip.Color(0,150,0);
-  colors[2] = strip.Color(0,0,150);
-  strip.setBrightness(64);
+  colors[0] = strip.Color(255,0,0);
+  colors[1] = strip.Color(255,255,0);
+  colors[2] = strip.Color(0,255,0);
+  colors[3] = strip.Color(0,255,255);
+  colors[4] = strip.Color(0,0,255);
+  colors[5] = strip.Color(255,0,255);
+  strip.setBrightness(32);
 }
 
 void loop() {
   if (Serial.available() > 0){
-    num_agents=3;
     int signal = Serial.read()-48;
-    Serial.println(signal);
 
-    if (signal == -100000){
-      num_agents = Serial.parseInt();
-      Serial.println(num_agents);
+    if (signal == 9){
+      num_agents = Serial.read()-48;
+      num_channels = Serial.read()-48;
 
     }
     else{
@@ -34,16 +36,13 @@ void loop() {
       //read strategies for timestep
       for (int i=0; i < num_agents; i++){
         agent_strategies[i] = (Serial.read() - 48);
-        Serial.print(i);
-        Serial.print(" ");
-        Serial.println(agent_strategies[i]);
         
         int current_led = signal*8 + i;
         if (agent_strategies[i] ==9){ //if agent not trasmitting
           strip.setPixelColor(current_led, 150,150,150); //no LED
         }
         else{
-          strip.setPixelColor(current_led, colors[agent_strategies[i]]);
+          strip.setPixelColor(current_led, colors[agent_strategies[i]*6/num_channels]);
         }
       }
     }
